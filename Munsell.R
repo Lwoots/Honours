dc1744682d8f5cb77adc804e69e3ad0ee58882bc
Chopin <larawootton@gmail.com>
2018-03-21 18:49:34 +0200

    Rewriting with new data. Getting rind or uneven numbers

--- Munsell.R
+++ Munsell.R
@@ -22,16 +22,20 @@ setwd("/Users/larawootton/Documents/Honours")
 soil_col <- read.csv("Munsell_8Mar.csv", sep = ";")
 grid <- subset(soil_col, type == "grid")
 
+rownames(grid) <- 1:length(grid$munsell)
+col <- grid$munsell[25:36]
 
-col <- soil_col$munsell[4:39]
+grid$munsell[grid$munsell == "  "] <- NA
 
 plot_mnsl(col)
 plot_mnsl(grid$munsell[1:20], na.rm = T)
 
-p <- plot_mnsl
+p <- plot_mnsl(col)
 summary(p)
 
-p+ggplot2::facet_wrap(~soil_col$munsell[4:46], ncol = 6)
+p + ggplot2::facet_wrap(~ num, nrow = 2)
+
+p+ggplot2::facet_wrap(~soil_col$munsell[4:46], nc = 6)
 ?plot_mnsl
 
 sample()
