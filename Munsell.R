466b4c4cdeca72d45da7b7741a90ec7f0ba7fb4d
Chopin <larawootton@gmail.com>
2018-03-21 19:40:37 +0200

    Wrangling the data to make a missing row where R40 was

--- Munsell.R
+++ Munsell.R
@@ -1,33 +1,42 @@
-require("munsell")
 
-col <- c("10YR 4/6",
-"10YR 5/6",
-"10YR 6/6",
-"10YR 5/8",
-"10YR 6/6",
-"10YR 6/6",
-"10YR 5/4",
-"10YR 4/6",
-"7.5YR 5/6",
-"10YR 6/6",
-"10YR 6/6",
-"10YR 6/6",
-"7.5YR 5/6",
-"10YR 5/4",
-"10YR 6/4",
-"10YR 6/6",
-"10YR 6/4")
+if(!require(pacman)){install.packages("pacman", dependencies=TRUE); library(pacman)}
+p_load(dplyr, ggplot2, munsell)
 
-setwd("/Users/larawootton/Documents/Honours")
-soil_col <- read.csv("Munsell_8Mar.csv", sep = ";")
+setwd("/Users/larawootton/Documents/Honours/Data")
+
+### Creating the soil colour grids 21 March ###
+
+
+soil_col <- read.csv("Soil_texture_Final.csv", sep = ";")
 grid <- subset(soil_col, type == "grid")
 
 rownames(grid) <- 1:length(grid$munsell)
-col <- grid$munsell[25:36]
 
-grid$munsell[grid$munsell == "  "] <- NA
+plot_mnsl(grid$munsell) #Doesn't like uneven numbers
 
-plot_mnsl(col)
+grid$munsell[grid$munsell == "10YR 7/3"] <- "10YR 7/4"
+grid$munsell[grid$munsell == "7.5YR 6/3"] <- "7.5YR 6/4"
+
+
+
+#Site 3####
+
+plot_mnsl(grid$munsell[c(15:36,2,37, 3:14)])
+
+#Site 1 ####
+
+plot_mnsl(grid$munsell[38:73])
+
+#Site 2 ####
+dat <- data.frame(rbind(grid$munsell[c(74:90,103:106)], NA, grid$munsell[c(107, 91:102)]))
+
+temp <- matrix(rep(NA,14), nrow = 1)
+colnames(temp) <- colnames(grid)
+dat <- rbind(grid[c(74:84, 1, 85:90,103:106),],temp, grid[c(107, 91:102),])
+
+plot_mnsl(dat$munsell)
+
+
 plot_mnsl(grid$munsell[1:20], na.rm = T)
 
 p <- plot_mnsl(col)
