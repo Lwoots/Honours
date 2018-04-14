0070eedbf1b1fb0482aaabc49d42b6755f5738d3
Chopin <larawootton@gmail.com>
2018-04-14 16:06:08 +0200

    Descriptive stats for texture

--- Explore_tex_ph_14Apr.R
+++ Explore_tex_ph_14Apr.R
@@ -11,17 +11,83 @@ p_load(dplyr, visdat)
 
 plots <- read.csv("plots.csv")
 sieved <- read.csv("Soil_texture_Final.csv", sep = ";") 
-malvern <- read.csv("Raw_Malvern_data_for_analysis_31Mar.csv", sep = ";")
+malvern <- read.csv("Malvern_dodge_labels.csv", sep = ";")
+ph <- read.csv("ph_cond_13Apr.csv", sep = ";")
+
 
 which(duplicated(malvern$Sample) == TRUE) #Find the duplicated samples in the malvern data
-
-malvern <- malvern[-c(156, 153, 152, 65, 81, 95, 99, 103),]
-
-
-
-?unique
-duplicated
 
 setdiff(plots$plot, sieved$plot)
 setdiff(plots$plot, malvern$Sample)
 setdiff(malvern$Sample, sieved$plot)
+
+names(malvern)[1] <- "plot"
+names(ph)[1] <- "plot"
+
+texture <- left_join(sieved, malvern, by = "plot")
+
+texture <- texture %>% select(1:12, 127:133)
+glimpse(texture)
+
+texture <- left_join(texture, ph, by = "plot")
+texture <- mutate(texture, 
+                  Sand = Fine_sand + Medium_sand + Coarse_sand + Very_coarse_sand,
+                  prop_less2 = (between2_1/soil_mass)*100, 
+                  prop_less5 = (between2_5mm/soil_mass)*100)
+
+grid_texture <- texture %>% filter(type == "grid") %>% arrange(plot)
+
+site3 <- grid_texture[7:42,] %>% 
+                     mutate(x = as.vector(c(rep(1,6), rep(3,6), rep(5,6), rep(7,6), rep(9,6), rep(11,6))),
+                            y = as.vector(rep(seq(1, 11, 2), 6)))
+
+site1 <- grid_texture[c(1:6, 43:72),] %>% 
+                      mutate(x = as.vector(c(rep(1,6), rep(3,6), rep(5,6), rep(7,6), rep(9,6), rep(11,6))),
+                             y = as.vector(rep(seq(1, 11, 2), 6)))
+
+site2 <- grid_texture[73:111,]
+
+rm(ph, malvern, plots, sieved)
+
+#Descriptive stats
+names(texture)
+clay <- texture %>% summarise(avg = mean(Clay), 
+                              max = max(Clay), 
+                              min = min(Clay), 
+                              sd = sd(Clay))
+silt <- texture %>% summarise(avg = mean(Silt), 
+                              max = max(Silt), 
+                              min = min(Silt), 
+                              sd = sd(Silt))   
+very_fine_sand <- texture %>% summarise(avg = mean(Very_fine_sand), 
+                                        max = max(Very_fine_sand), 
+                                        min = min(Very_fine_sand), 
+                                        sd = sd(Very_fine_sand))
+medium_sand <- texture %>% summarise(avg = mean(Medium_sand), 
+                                     max = max(Medium_sand), 
+                                     min = min(Medium_sand), 
+                                     sd = sd(Medium_sand))
+coarse_sand <- texture %>% summarise(avg = mean(Coarse_sand), 
+                                     max = max(Coarse_sand), 
+                                     min = min(Coarse_sand), 
+                                     sd = sd(Coarse_sand))
+very_coarse_sand <- texture %>% summarise(avg = mean(Very_coarse_sand), 
+                                          max = max(Very_coarse_sand), 
+                                          min = min(Very_coarse_sand), 
+                                          sd = sd(Very_coarse_sand))
+total_sand <- texture %>% summarise(avg = mean(Sand), 
+                                 max = max(Sand), 
+                                 min = min(Sand), 
+                                 sd = sd(Sand))
+less2 <- texture %>% summarise(avg = mean(prop_less2), 
+                               max = max(prop_less2), 
+                               min = min(prop_less2), 
+                               sd = sd(prop_less2))
+less5 <- texture %>% summarise(avg = mean(prop_less5),
+                               
+                               max = max(prop_less5), min = min(prop_less5), 
+                               sd = sd(prop_less5))
+
+tex_des <- rbind(clay, silt, very_fine_sand, medium_sand, coarse_sand, very_coarse_sand, total_sand, less2, less5)
+category <- c("clay", "silt", "very_fine_sand", "medium_sand", "coarse_sand", "very_coarse_sand","total_sand", "less2", "less5")
+data.frame(category, tex_des)
