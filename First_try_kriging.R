e59f10bf743e9fb95b4d4f7c5616c0fad1beeb17
Chopin <larawootton@gmail.com>
2018-04-22 19:17:36 +0200

    Starting to krig

--- First_try_kriging.R
+++ First_try_kriging.R
@@ -8,10 +8,12 @@ rm(list = ls())
 setwd("/Users/larawootton/Documents/Honours/Data")
 
 if(!require(pacman)){install.packages("pacman", dependencies=TRUE); library(pacman)}
-p_load(dplyr, ggplot2, RColorBrewer, sp, gstat)
+p_load(dplyr, ggplot2, RColorBrewer, sp, gstat, automap)
 
 ph <- read.csv("pH_for_analysis.csv", sep = ";")
 raw <- read.csv("Species_abundance_for_analysis.csv", sep = ";")
+
+#Data wrangling ####
 
 #Extract the names of all the plots
 plots <- data.frame(raw[,2])
@@ -37,4 +39,74 @@ site1_full_grid <- full_grid %>% filter(site == "site1")
 
 site1_measured <- measured %>% filter(site == "site1", type == "grid")
 
-#
+
+
+#Now for the actual kriging####
+
+#convert to spatial dataframe
+
+coordinates(site1_measured) <- c("x", "y")
+
+#Create a semivariogram
+
+ph_vgm <- variogram(ph_kcl ~ 1, site1_measured)
+plot(ph_vgm) #From here you could fit your own model using fit.variogram
+
+#Fitting a model automatically
+
+auto_ph <- autofitVariogram(ph_kcl ~ 1, site1_measured)
+summary(auto_ph)
+
+plot(auto_ph)
+
+#To auto Krige
+coordinates(site1_full_grid) <- c("x", "y")
+
+ph_autoKrige <- autoKrige(ph_kcl ~ 1, site1_measured, site1_full_grid)
+
+plot(ph_autoKrige)
+glimpse(ph_autoKrige)
+ph_autoKrige@var1.pred
+
+pred_krige <- as.data.frame(ph_autoKrige[1])
+
+p <- ph_autoKrige[1] %>% as.data.frame %>%
+  ggplot(aes(x=krige_output.x, y=krige_output.y)) + geom_tile(aes(fill=krige_output.var1.pred)) + coord_equal() +
+  scale_fill_gradient(low = "yellow", high="red") +
+  #scale_x_continuous(labels=comma) + scale_y_continuous(labels=comma) +
+  theme_bw() +
+  theme(legend.position = "none")
+
+test <- measured %>% filter(site == "site1", type == "random")
+
+p2 <- ggplot(test,
+       aes(x,y)
+       ) + 
+  geom_tile(aes(fill = ph_kcl)) +
+  scale_fill_gradient(low = "yellow", high="red", limits = c(min(pred_krige$krige_output.var1.pred), max(pred_krige$krige_output.var1.pred)))
+
+t.test(test$ph_kcl, pred_krige$krige_output.var1.pred)
+plot_grid(p,p2)
+
+
+#Try with logging
+
+log_auto_ph <- autofitVariogram(log(ph_kcl) ~ 1, site1_measured)
+log_ph_autoKrige <- autoKrige(log(ph_kcl) ~ 1, site1_measured, site1_full_grid)
+log_pred_krige <- as.data.frame(log_ph_autoKrige[1])
+
+p <- log_ph_autoKrige[1] %>% as.data.frame %>%
+  ggplot(aes(x=krige_output.x, y=krige_output.y)) + geom_tile(aes(fill=krige_output.var1.pred)) + coord_equal() +
+  scale_fill_gradient(low = "yellow", high="red") +
+  #scale_x_continuous(labels=comma) + scale_y_continuous(labels=comma) +
+  theme_bw() +
+  theme(legend.position = "none")
+
+p2 <- ggplot(test,
+             aes(x,y)
+) + 
+  geom_tile(aes(fill = log(ph_kcl))) +
+  scale_fill_gradient(low = "yellow", high="red", limits = c(min(log_pred_krige$krige_output.var1.pred), max(log_pred_krige$krige_output.var1.pred)))
+
+t.test(test$ph_kcl, pred_krige$krige_output.var1.pred)
+plot_grid(p,p2)
