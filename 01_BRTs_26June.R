6ee557934f7d00f0bb8c6fc3207ad5cd4b092de8
Chopin <larawootton@gmail.com>
2018-06-26 12:03:48 +0200

    delaetii opt

--- 01_BRTs_26June.R
+++ 01_BRTs_26June.R
@@ -3,7 +3,7 @@ setwd("/Users/larawootton/Documents/Honours/Data")
 source("/Users/larawootton/Documents/Honours/Project_analysis/to_be_sourced.R")
 
 if(!require(pacman)){install.packages("pacman", dependencies=TRUE); library(pacman)}
-p_load(dplyr, dismo, gbm)
+p_load(dplyr, dismo, gbm, foreach, doParallel, TeachingDemos)
 
 glimpse(my_soil_df)
 
@@ -56,7 +56,86 @@ brt_results <- gbm.step(my_prez,
 summary(brt_results)
 gbm.plot(brt_results)
 
-
-
 brt.simp <- gbm.simplify(brt_results, n.drops = "auto")
 summary(brt.simp)
+
+
+#Find optimal settings for BRTs - code courtesy Mike
+
+cores <- detectCores()
+
+#Create training function for gbm.step
+
+step.train.fx = function(tree.com, learn){
+  #set seed for reproducibility
+  char2seed("StackOverflow", set = TRUE)
+  k1 <- gbm.step(data = my_prez, 
+                 gbm.x = brt_var, 
+                 gbm.y = 7,
+                 family = "bernoulli", 
+                 tree.complexity = tree.com,
+                 learning.rate = learn,
+                 bag.fraction = 0.7,
+                 prev.stratify=TRUE,
+                 n.folds=10,
+                 step.size=50,
+                 silent=FALSE,
+                 plot.main = TRUE,
+                 n.cores= cores - 1)
+  
+  k.out=list(interaction.depth=k1$interaction.depth,
+             shrinkage=k1$shrinkage,
+             n.trees=k1$n.trees,
+             AUC=k1$self.statistics$discrimination,
+             cv.AUC=k1$cv.statistics$discrimination.mean,
+             deviance=k1$self.statistics$mean.resid,
+             cv.deviance=k1$cv.statistics$deviance.mean)  
+  return(k.out)
+}
+
+#Setup to determine the optimal learning rate and tree complexity 
+#define complexity and learning rate
+tree.complexity<-c(1:5)
+learning.rate<-c(0.01,0.005,0.001,0.0005,0.0001)
+
+#Optimise for the current data
+#setup parallel backend to use n processors
+cl<-makeCluster(cores - 1)
+registerDoParallel(cl)
+
+#Run the actual function
+foreach(l = tree.complexity) %do% {
+  foreach(j = learning.rate) %do% {
+    nam=paste0("gbm_tc",l,"lr",j)
+    assign(nam,step.train.fx(tree.com=l,learn=j))
+  }
+}
+
+#Stop parallel
+stopCluster(cl)
+registerDoSEQ()
+
+#disable scientific notation
+options(scipen=999)
+
+#Find all item in workspace that contain "gbm_tc"
+train.all<-ls(pattern="gbm_tc")
+
+#cbind each list that contains "gbm_tc"
+train.results<-list(do.call(cbind,mget(train.all)))
+
+#Place in a data frame
+train.results<- do.call(rbind, lapply(train.results, rbind))
+train.results <- data.frame(matrix(unlist(train.results),ncol=7 , byrow=T))
+
+#Change column names
+colnames(train.results)<-c("TC","LR","n.trees", "AUC", "cv.AUC", "dev", "cv.dev")
+
+#Round 4:7
+train.results[,4:7]<-round(train.results[,4:7],digits=3)
+
+#Sort by cv.dev, cv.AUC, AUC
+train.results_out <-train.results[order(train.results$cv.dev,-train.results$cv.AUC, -train.results$AUC),]
+names(train.results_out)
+
+write.csv(train.results_out, "/Users/larawootton/Documents/Honours/Data/BRT_outputs/A_delaetii_ben_train_results_out")
