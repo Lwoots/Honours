99d6e292d9fccc26f8412b56513a445d08807d3b
Chopin <larawootton@gmail.com>
2018-08-06 15:20:41 +0200

    I made pretty graphs!

--- Boral_10June.R
+++ Boral_10June.R
@@ -8,28 +8,46 @@ rm(list = ls())
 setwd("/Users/larawootton/Documents/Honours/Data")
 
 if(!require(pacman)){install.packages("pacman", dependencies=TRUE); library(pacman)}
-p_load(dplyr,visdat, ggplot2, RColorBrewer, boral, rjags, mvabund)
+p_load(dplyr,visdat, ggplot2, RColorBrewer, boral, rjags, mvabund, corrplot)
 
 
 source("/Users/larawootton/Documents/Honours/Project_analysis/to_be_sourced.R")
 
-species_df <- species_df %>% select(-c(Q_cover, X, site, transect, lampranthus))
+nb_sp <- species_df %>% left_join(my_soil_df, by = "plot") %>% filter(!is.na(type)) %>%  select(plot, ruschia_burtoniae, c_spissum, a_delaetii, oophytum, a_fissum, dicrocaulon, drosanthemum_diversifolium, a_framesii, mesemb_1, c_subfenestratum, co_calculus, galenia_fruticosa, crassula_muscosa, tylecodon_pygmaeus, cephalophyllum_staminodiosum) 
+
+enviro_var <- my_soil_df %>% 
+  select(c(1, 13:21, 27:28, 30:31, 34, 37:42))
 
 
-species_df <- species_df %>% select(1:28)
-sp_pres <- species_df[,-c(1:3)] %>% apply(2, function(x) ifelse(is.na(x), 0, 1))
+sp_occ <- nb_sp %>% apply(2, function(x) ifelse(is.na(x), 0, 1))
 
+all_dat <- cbind(sp_occ, enviro_var)
+all_dat <- na.omit(all_dat) %>% select(-plot)
 
 #Covariates
-my_soil_df <- my_soil_df %>% filter(!is.na(type))
+covar <- all_dat %>% 
+  select(c(16:35)) %>% 
+  as.matrix()
 
-covar <- as.matrix(my_soil_df[,c(13:21, 27:28, 30:31, 34, 37)])
+sp <- as.matrix(all_dat[,1:15])
 
-mod1 <- boral(sp_pres, X = covar, family = "binomial")
+mod1 <- boral(sp, 
+              X = covar,
+              family = "binomial",
+              num.lv = 3,
+              save.model = T)
+
 summary(mod1)
 lvsplot(mod1)
+coefsplot(mod1)
 
+envcors <- get.enviro.cor(mod1)
+rescors <- get.residual.cor(mod1)
 
+corrplot(envcors$cor)
+corrplot(envcors$sig.cor)
+corrplot(rescors$correlation, order = "AOE", type = "lower")
+corrplot.mixed(rescors$sig.correlaton)
 
 
 mod2 <- boral(sp_pres, num.lv = 3, family = "binomial")
